9a70ac7073e3fb4e2b8df64d646620f9ee681378
Henning Schmiedehausen <henning.schmiedehausen@zuora.com>
2018-01-19 17:08:02 -0800

    asl headers

--- src/main/java/de/softwareforge/jsonschema/annotations/SchemaIgnore.java
+++ src/main/java/de/softwareforge/jsonschema/annotations/SchemaIgnore.java
@@ -1,21 +1,16 @@
 /*
- * Copyright (c) 2014, Danilo Reinert (daniloreinert@growbit.com)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This software is dual-licensed under:
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
- *   later version;
- * - the Apache Software License (ASL) version 2.0.
- *
- * The text of both licenses is available under the src/resources/ directory of
- * this project (under the names LGPL-3.0.txt and ASL-2.0.txt respectively).
- *
- * Direct link to the sources:
- *
- * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
- * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package de.softwareforge.jsonschema.annotations;
 
 import java.lang.annotation.ElementType;
